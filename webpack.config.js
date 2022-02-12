c2f7a0a1f6b95bebb4cabe54fdf5f8534693f776
kolserdav <serega12101983@gmail.com>
2022-02-12 22:31:44 +0700

    Added dev server, set up eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,26 @@
+// @ts-check
+
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PUBLIC_DIR = 'dist';
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, PUBLIC_DIR),
+    filename: 'js/bundle.js',
   },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+  		filename: 'index.html',
+      template: 'src/index.html',
+    }),
+  ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, PUBLIC_DIR),
     },
     compress: true,
     port: 9000,
